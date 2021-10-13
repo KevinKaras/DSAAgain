5649103bd39fc0832632ea9cf8ca094cb6b81924
KevinKaras <kevinkaras2@gmail.com>
2021-10-13 12:24:04 -0400

    added valid parentheses solution in php

---
+++ PHPValidParentheses.js
@@ -0,0 +1,24 @@
+function isValid($s) {
+        $parentheses = [
+            "(" => ")",
+            "[" => "]",
+            "{" => "}",
+        ];
+        
+        $stack = array();
+        
+        for($i = 0; $i < strlen($s); $i++) {
+            if (isset($parentheses[$s[$i]])) {
+                array_push($stack, $s[$i]);
+            } else {
+                $parenthesis = array_pop($stack);
+        
+                if ($s[$i] != $parentheses[$parenthesis]) {
+                    return false;
+                }
+            }
+        }
+        
+        return empty($stack);
+}
+
