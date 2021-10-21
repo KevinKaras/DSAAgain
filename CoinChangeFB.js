7720671a0f51fa80e6845a9c616327bc22e6ea4c
KevinKaras <kevinkaras2@gmail.com>
2021-10-21 18:53:17 -0500

    removed unneeded console logs

--- CoinChangeFB.js
+++ CoinChangeFB.js
@@ -8,17 +8,17 @@ function coinChange(array, amount){
     for(let i = 0; i < amount + 1; i++){
         dp.push(amount+1)
     }
-    console.log("dp", dp)
+    
     dp[0] = 0 
 
     for(let i = 1; i < (amount + 1); i++){
-        console.log("i", i)
+        
         for(let j = 0; j < array.length; j++){
-            console.log("j", j)
+            
 
             if((i - array[j]) >= 0){
                 dp[i] = Math.min(dp[i], 1 + dp[i - array[j]])
-                console.log("current dp", dp[i])
+                
             }
 
         }
