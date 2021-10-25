0d581c3a4df2931947f2cc48d4bfebf9f6ed3828
KevinKaras <kevinkaras2@gmail.com>
2021-10-25 16:30:21 -0500

    added breath first traversal iterative

---
+++ Javascript/TREES/breathFirstTravel.js
@@ -0,0 +1,46 @@
+class Node{
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+//  ITERATIVE
+
+
+function breathFirstTravelIterative(root){
+    if(root === null){
+        return []
+    }
+    let finalArr = []
+    let queue = [ root ]
+    while(queue.length > 0){
+
+        let current = queue.pop()
+        finalArr.push(current)
+
+        if(current.right) queue.unshift(current.right)
+        if(current.left)  queue.unshift(current.left)
+
+
+    }
+    return finalArr
+}
+
+console.log(breathFirstTravelIterative(a))
+
