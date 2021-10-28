cd972c20f12db8cfd4372553840423bef8a4bc38
KevinKaras <kevinkaras2@gmail.com>
2021-10-27 20:39:39 -0500

    change valid parentheses problem to have no errors v3 good grief

--- PHPValidParentheses.php
+++ PHPValidParentheses.php
@@ -1,29 +1,34 @@
 
 <?php 
 
-$isValid($s) {
-        $parentheses = [
-            "(" => ")",
-            "[" => "]",
-            "{" => "}",
-        ];
-        
-        $stack = array();
-        
-        for($i = 0; $i < strlen($s); $i++) {
-            if (isset($parentheses[$s[$i]])) {
-                array_push($stack, $s[$i]);
-            } else {
-                $parenthesis = array_pop($stack);
-        
-                if ($s[$i] != $parentheses[$parenthesis]) {
-                    return false;
-                }
-            }
+function isValid($s) {
+    $s = trim($s);
+    if (!$s) {
+      return true;
+    }
+    if (strlen($s) === 1) {
+      return false;
+    }
+
+    $brackets = [
+      '[' => ']',
+      '(' => ')',
+      '{' => '}',
+    ];
+
+    for ($stack = [], $length = strlen($s), $i = 0; $i < $length; $i++) {
+      $symbol = $s[$i];
+      if (array_key_exists($symbol, $brackets)) {
+        $stack[] = $symbol;
+      } else {
+        $lastInStack = array_pop($stack);
+        if (!isset($brackets[$lastInStack]) || $symbol !== $brackets[$lastInStack]) {
+          return false;
         }
-        
-        return empty($stack);
-}
+      }
+    }
+    return (count($stack) === 0) ? true : false;
+  }
 ?>
 
 // rework this to be errorless
